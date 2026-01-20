10d61457ff61f80ff2fc4f3046af31e5ed099486
Alvaro <alvarojara.dev@gmail.com>
2026-01-20 08:03:00 -0300

    refactor(networking): migrate to private networking module

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This repository is used to explore Terraform Cloud's VCS integration
 
 - The variables are created within the Terraform Cloud
 - The push automatically triggers an apply in Terraform Cloud, you can also modify this configuration to add auto-approve or more.
+
+### Merge - Speculative Plan - Apply
+
+- Merge pull request → You can click on View details to see the Speculative Plan → After choosing confirm mergue request you can see an apply plan executed on Terraform.
--- networking.tf
+++ networking.tf
@@ -1,14 +1,41 @@
-resource "aws_vpc" "tf_cloud" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = "terraform-cloud"
+moved {
+  from = aws_vpc.tf_cloud
+  to   = module.vpc.aws_vpc.this
+}
+moved {
+  from = aws_subnet.tf_cloud
+  // migrating a single subnet
+  to = module.vpc.aws_subnet.this["subnet_1"]
+}
+
+module "vpc" {
+  source  = "app.terraform.io/terraform-org-ajdev/vpc/aws"
+  version = "0.0.1"
+  # insert required variables here
+  vpc_config = {
+    cidr_block = var.vpc_cidr
+    name       = "terraform-cloud"
+  }
+
+  subnet_config = {
+    subnet_1 = {
+      cidr_block = "10.0.0.0/24"
+      az         = "us-east-1a"
+    }
   }
 }
 
-resource "aws_subnet" "tf_cloud" {
-  cidr_block = var.subnet_cidr
-  vpc_id     = aws_vpc.tf_cloud.id
-  tags = {
-    Name = "terraform-cloud"
-  }
-}
+# resource "aws_vpc" "tf_cloud" {
+#   cidr_block = var.vpc_cidr
+#   tags = {
+#     Name = "terraform-cloud"
+#   }
+# }
+
+# resource "aws_subnet" "tf_cloud" {
+#   cidr_block = var.subnet_cidr
+#   vpc_id     = aws_vpc.tf_cloud.id
+#   tags = {
+#     Name = "terraform-cloud"
+#   }
+# }
